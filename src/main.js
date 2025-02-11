7c1f1dc9dc21d7fa2fef3b10dab1cfe555f0f94d
DenysKors <105918404+DenysKors@users.noreply.github.com>
2025-02-11 19:53:57 +0200

    Add root styles

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Landing Page!</h1>
-  </div>
-`;
+const appRef = document.querySelector('#app');
+console.log(appRef);
