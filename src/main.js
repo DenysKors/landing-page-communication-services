245db2e10b6a5f3e6322a113fcdf3382f783b5a5
DenysKors <105918404+DenysKors@users.noreply.github.com>
2025-02-14 16:59:48 +0200

    Add switch theme logic, change colors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import './style.css';
 
-const appRef = document.querySelector('#app');
-console.log(appRef);
+const themeBtnRef = document.getElementById('theme-switch');
+// const mobMemuBtnRef = document.getElementById('#menu');
+console.log(themeBtnRef);
+
+const toggleTheme = () => {
+  document.body.classList.toggle('dark-theme');
+};
+
+themeBtnRef.addEventListener('click', toggleTheme);
