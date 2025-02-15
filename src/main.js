fa46345b60424334b351b51ea135b38327ba9d99
DenysKors <105918404+DenysKors@users.noreply.github.com>
2025-02-15 18:26:26 +0200

    Add theme icon toggle logic

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import './style.css';
 
 const themeBtnRef = document.getElementById('theme-switch');
+const svgLightIconRef = document.getElementById('icon-light');
+const svgDarkIconRef = document.getElementById('icon-dark');
 // const mobMemuBtnRef = document.getElementById('#menu');
-console.log(themeBtnRef);
 
 const toggleTheme = () => {
   document.body.classList.toggle('dark-theme');
+
+  if (document.body.classList.contains('dark-theme')) {
+    svgLightIconRef.classList.add('hide');
+    svgDarkIconRef.classList.remove('hide');
+  } else {
+    svgLightIconRef.classList.remove('hide');
+    svgDarkIconRef.classList.add('hide');
+  }
 };
 
 themeBtnRef.addEventListener('click', toggleTheme);
